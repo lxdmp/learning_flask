591ab51b3c5a2433c91fcc85037a866af8eaaa90
minpeng.deng <yudmp@sina.com>
2017-09-03 19:25:25 +0800

    test global/local(inside blueprint) error handler

--- app/__init__.py
+++ app/__init__.py
@@ -30,13 +30,28 @@ def create_app(config_name):
 	app.jinja_env.filters['datetime'] = custom_filter_datetime
 
 	# 蓝本注册
-	from .blog import blog as blog_blueprint
-	app.register_blueprint(blog_blueprint)
+	from .blog import blog as blog_bp
+	app.register_blueprint(blog_bp)
+
+	from .admin import admin as admin_bp
+	app.register_blueprint(admin_bp)
 
 	# 首页重定向
 	@app.route('/')
 	def index():
 		return flask.redirect(flask.url_for('blog.index'))
+
+	# 错误重定向
+	@app.errorhandler(404)
+	def page_not_found(error):
+		return flask.render_template(
+			"page_not_found.html", 
+			target_url=flask.url_for('blog.index'), 
+			delay_sec=4), 404
+	
+	@app.errorhandler(500)
+	def internal_error(error):
+		return flask.render_template("internal_error.html"), 500
 	
 	# 请求预处理(若访问蓝图,将蓝图的路径设为优先搜索路径)
 	'''
--- app/admin/errors.py
+++ app/admin/errors.py
@@ -1,15 +1,14 @@
 # coding=utf-8
 import flask
-from . import blog
+from . import admin
 
-@blog.errorhandler(404)
+@admin.errorhandler(404)
 def page_not_found(error):
 	return flask.render_template(
 		"page_not_found.html", 
-		target_url=flask.url_for('blog.index'), 
-		delay_sec=3), 404
+		target_url=flask.url_for('admin.index'), 
+		delay_sec=5), 404
 
-@blog.errorhandler(500)
+@admin.errorhandler(500)
 def internal_error(error):
 	return flask.render_template("internal_error.html"), 500
-
